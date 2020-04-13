d8867f4d7fbb247d05826189fb8111153c981e78
Gabe De Luca <me@gabe.io>
2020-04-13 02:15:43 -0400

    adds cursive

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,27 @@
+use cursive::Cursive;
+use cursive::views::{TextView};
+
 mod ohhell;
 
 fn main() {
     let ohhell: ohhell::Ohhell = ohhell::create_ohhell();
     ohhell.launch();
+
+    let mut siv = Cursive::default();
+
+    // We can quit by pressing `q`
+    siv.add_global_callback('q', Cursive::quit);
+
+    // Add a simple view
+    siv.add_layer(TextView::new(
+        "Hello World!\n\
+        ♦\n\
+        ♣\n\
+        ♠\n\
+        ♥\n\
+         Press q to quit the application.",
+    ));
+
+    // Run the event loop
+    siv.run();
 }
