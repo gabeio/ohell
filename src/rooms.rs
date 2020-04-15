cd043808174bf61fae79feab49425e7ea3072347
Gabe D <me@gabe.io>
2020-04-15 02:45:36 -0400

    adds rooms

---
+++ src/rooms.rs
@@ -0,0 +1,25 @@
+#[path = "./ohhell.rs"] mod ohhell;
+
+use uuid::Uuid;
+
+#[derive(Debug)]
+#[derive(Clone)]
+pub struct Room {
+    uuid: Uuid,
+    game: ohhell::Ohhell,
+}
+
+impl Room {
+    //
+    pub fn new() -> Room {
+        Room {
+            uuid: Uuid::new_v4(),
+            game: ohhell::Ohhell::new(),
+        }
+    }
+
+    pub fn start(mut self) -> Room {
+        self.game = self.game.start();
+        self
+    }
+}
